477d173b3e9d6596a37cb05f5fb57c79b4d7dfde
Noah Huppert <contact@noahh.io>
2019-04-20 17:09:50 -0400

    added create user flags to main

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"database/sql"
 		
 	"github.com/WWPOL/Game-Deals/config"
+	"github.com/WWPOL/Game-Deals/models"
 
 	_ "github.com/lib/pq"
         "github.com/jmoiron/sqlx"
@@ -32,6 +33,7 @@ func main() {
 	}
 
 	// {{{1 Connect to database
+	// {{{2 Raw database
 	db, err := sql.Open("postgres",
 		fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable",
 			cfg.Database.Host,
@@ -45,13 +47,31 @@ func main() {
 		logger.Fatalf("failed to ping database: %s", err.Error())
 	}
 
-	// {{{1 Migrate if -m migrate flag provided
+	// {{{1 Sqlx
+	dbx := sqlx.NewDb(db, "postgres")
+
+	// {{{1 Perform utility actions
+	// {{{2 Parse flags
+	var createUserName string
+	flag.StringVar(&createUserName, "u", "", "Create user with name")
+
+	var createUserPassword string
+	flag.StringVar(&createUserPassword, "p", "", "Create user with password")
+
 	var migrateMode bool
 	flag.BoolVar(&migrateMode, "m", false, "Run database migrations and exit")
-	
+
 	flag.Parse()
 
+	// {{{2 Check flags aren't competing
+	if len(createUserName) > 0 && len(createUserPassword) > 0 && migrateMode {
+		logger.Fatal("-m flag cannot be provided with -u and -p flags")
+	}
+
+	// {{{2 Migrate if -m migrate flag provided
 	if migrateMode {
+		logger.Info("running migrations")
+		
 		driver, err := postgres.WithInstance(db, &postgres.Config{})
 		if err != nil {
 			logger.Fatalf("failed to create Postgres migration driver: %s",
@@ -72,7 +92,38 @@ func main() {
 		os.Exit(0)
 	}
 
-	// {{{1 Setup sqlx if not migrating
-	dbx := sqlx.NewDb(db, "postgres")
-	logger.Debugf("debug: %#v", dbx)
+	// {{{2 Create user if -u username:password flag provided
+	if len(createUserName) > 0 && len(createUserPassword) > 0 {
+		tx, err := dbx.Beginx()
+		if err != nil {
+			logger.Fatalf("failed to create transaction for user insert: %s",
+				err.Error())
+		}
+
+		passwordHash, err := models.HashPassword(createUserPassword)
+		if err != nil {
+			logger.Fatalf("failed to hash user password: %s", err.Error())
+		}
+
+		user := models.User{
+			Username: createUserName,
+			PasswordHash: passwordHash,
+		}
+
+		err = tx.QueryRowx("INSERT INTO users (username, password_hash) "+
+			"values ($1, $2) RETURNING id",
+			user.Username, user.PasswordHash).StructScan(&user)
+
+		if err != nil {
+			logger.Fatalf("failed to insert user: %s", err.Error())
+		}
+
+		if err = tx.Commit(); err != nil {
+			logger.Fatalf("failed to commit user insert: %s", err.Error())
+		}
+
+		logger.Infof("Inserted user %s with ID %d", user.Username, user.ID)
+
+		os.Exit(0)
+	}
 }
