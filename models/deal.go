d637f08458f71d1d24177a24a1b719384e35d5e0
Noah Huppert <contact@noahh.io>
2019-04-21 03:34:07 -0400

    models: deal: fixed insert method, added json tags to field

--- models/deal.go
+++ models/deal.go
@@ -10,28 +10,28 @@ import (
 // Deal on game
 type Deal struct {
 	// ID
-	ID int64 `db:"id"`
+	ID int64 `db:"id" json:"id"`
 	
 	// GameID is the ID of game deal relates to
-	GameID int `db:"game_id"`
+	GameID int `db:"game_id" validate:"required" json:"game_id"`
 
 	// Start is the date and time a deal starts on
-	Start *time.Time `db:"start_time"`
+	Start *time.Time `db:"start_time" json:"start_time"`
 
 	// End is the optional deal end date and time
-	End *time.Time `db:"end_time"`
+	End *time.Time `db:"end_time" json:"end_time"`
 
 	// Published is the optional published time
-	Published *time.Time `db:"published_time"`
+	Published *time.Time `db:"published_time" json:"published_time"`
 
 	// Price of game with deal, 0 if free
-	Price float64 `db:"price"`
+	Price float64 `db:"price" validate:"required" json:"price"`
 
 	// Link to game deal
-	Link string `db:"link"`
+	Link string `db:"link" validate:"required" json:"link"`
 
 	// Description is an optional description of the deal
-	Description string `db:"description"`
+	Description string `db:"description" validate:"required" json:"description"`
 }
 
 // QueryAllDeals return an array of deals in the database
@@ -56,7 +56,7 @@ func QueryAllDeals(dbx *sqlx.DB) ([]Deal, error) {
 }
 
 // Insert game into database
-func (g *Game) Insert(dbx *sqlx.DB) error {
+func (d *Deal) Insert(dbx *sqlx.DB) error {
 	tx, err := dbx.Beginx()
 	if err != nil {
 		return fmt.Errorf("error beginning transaction: %s", err.Error())
@@ -64,8 +64,8 @@ func (g *Game) Insert(dbx *sqlx.DB) error {
 
 	err = tx.QueryRowx("INSERT INTO deals (game_id, start_time, end_time, "+
 		"published_time, price, link, description) VALUES ($1, $2, $3, "+
-		"$4, $5, $6, $7) RETURNING id", g.GameID, g.Start, g.End, g.Published,
-	        g.Price, g.Link, g.Description).StructScan(g)
+		"$4, $5, $6, $7) RETURNING id", d.GameID, d.Start, d.End, d.Published,
+	        d.Price, d.Link, d.Description).StructScan(d)
 	if err != nil {
 		return fmt.Errorf("error executing query: %s", err.Error())
 	}
