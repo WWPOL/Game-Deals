25c768d89dec62ce71f021d1845a92396fe218e2
Noah Huppert <contact@noahh.io>
2019-04-20 23:38:37 -0400

    models: game: wrote QueryAllGames

--- models/game.go
+++ models/game.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"fmt"
+	
+	"github.com/jmoiron/sqlx"
+)
+
 // Game is a video game
 type Game struct {
 	// ID
@@ -7,4 +13,25 @@ type Game struct {
 	
 	// Name
 	Name string `db:"name"`
+}
+
+// QueryAllGames returns an array of Games in the database
+func QueryAllGames(dbx *sqlx.DB) ([]Game, error) {
+	games := []Game{}
+
+	rows, err := dbx.Queryx("SELECT * FROM games")
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute query: %s", err.Error())
+	}
+
+	for rows.Next() {
+		game := Game{}
+
+		if err = rows.StructScan(&game); err != nil {
+			return nil, fmt.Errorf("failed to scan row into struct: %s", err.Error())
+		}
+		games = append(games, game)
+	}
+
+	return games, nil
 }
