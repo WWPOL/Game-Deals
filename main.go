756d6e73c0910c7a50bea17e0de9546713f7f736
Noah Huppert <contact@noahh.io>
2019-04-22 17:53:43 -0400

    main: re-ordered route registration to be in C.R.U.D order

--- main.go
+++ main.go
@@ -169,18 +169,24 @@ func main() {
 
 	// {{{2 Games routes
 	gamesLogger := routesLogger.GetChild("games")
-	
-	r.Handle("/api/v0/games", routes.ListGamesHandler{
-		Logger: gamesLogger.GetChild("list"),
-		Config: cfg,
-		Dbx: dbx,
-	}).Methods("GET")
 
 	r.Handle("/api/v0/games", routes.CreateGameHandler{
 		Logger: gamesLogger.GetChild("create"),
 		Config: cfg,
 		Dbx: dbx,
 	}).Methods("POST")
+	
+	r.Handle("/api/v0/games", routes.ListGamesHandler{
+		Logger: gamesLogger.GetChild("list"),
+		Config: cfg,
+		Dbx: dbx,
+	}).Methods("GET")
+	
+	r.Handle("/api/v0/games/{id:[0-9]+}", routes.UpdateGameHandler{
+		Logger: gamesLogger.GetChild("update"),
+		Config: cfg,
+		Dbx: dbx,
+	}).Methods("UPDATE")
 
 	r.Handle("/api/v0/games/{id:[0-9]+}", routes.DeleteGameHandler{
 		Logger: gamesLogger.GetChild("delete"),
@@ -188,25 +194,19 @@ func main() {
 		Dbx: dbx,
 	}).Methods("DELETE")
 
-	r.Handle("/api/v0/games/{id:[0-9]+}", routes.UpdateGameHandler{
-		Logger: gamesLogger.GetChild("update"),
-		Config: cfg,
-		Dbx: dbx,
-	}).Methods("UPDATE")
-
 	// {{{2 Deals routes
 	dealsLogger := routesLogger.GetChild("deals")
-	
-	r.Handle("/api/v0/deals", routes.ListDealsHandler{
-		Logger: dealsLogger.GetChild("list"),
-		Dbx: dbx,
-	}).Methods("GET")
 
 	r.Handle("/api/v0/deals", routes.CreateDealHandler{
 		Logger: dealsLogger.GetChild("create"),
 		Config: cfg,
 		Dbx: dbx,
 	}).Methods("POST")
+	
+	r.Handle("/api/v0/deals", routes.ListDealsHandler{
+		Logger: dealsLogger.GetChild("list"),
+		Dbx: dbx,
+	}).Methods("GET")
 
 	r.Handle("/api/v0/deals/{id:[0-9]+}", routes.UpdateDealHandler{
 		Logger: dealsLogger.GetChild("update"),
