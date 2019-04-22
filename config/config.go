0677a759b64f890c1da38ab92d5d1d54f6fa17f6
Noah Huppert <contact@noahh.io>
2019-04-22 16:11:36 -0400

    config: added validate required fields, added firebase config

--- config/config.go
+++ config/config.go
@@ -5,24 +5,30 @@ type Config struct {
 	// Server configuration
 	Server struct {
 		// Port
-		Port int `default:"8000"`
+		Port int `default:"8000" validate:"required"`
 	}
 	
 	// Database configuration
 	Database struct {
 		// Host
-		Host string `default:"localhost"`
+		Host string `default:"localhost" validate:"required"`
 
 		// User
-		User string `default:"postgres"`
+		User string `default:"postgres" validate:"required"`
 
 		// Database
-		Database string `default:"game_deals"`
+		Database string `default:"game_deals" validate:"required"`
 	}
 
 	// Authentication configuration
 	Authentication struct {
 		// Signing secret
-		Secret string
+		Secret string `validate:"required"`
+	}
+
+	// Firebase configuration
+	Firebase struct {
+		// ServiceAccountFile is the path to a Firebase service account credentials file
+		ServiceAccountFile string `validate:"required"`
 	}
 }
