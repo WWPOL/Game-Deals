31e3f47479cfb80b4de8c0613f3e4a06a30cf84d
Noah Huppert <contact@noahh.io>
2019-04-21 21:12:41 -0400

    models: subscription: created migrations

---
+++ migrations/03_create_subscriptions_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE subscriptions (
+       id SERIAL PRIMARY KEY,
+       email TEXT NOT NULL,
+       notify_email BOOLEAN NOT NULL,
+       notify_push BOOLEAN NOT NULL,
+       verify_token TEXT NOT NULL,
+       verified BOOLEAN NOT NULL,
+       UNIQUE(email)
+)
