3402ea7da151880e48bc9545c0e39bacf70d1980
Rudhra Raveendran <rudytheninja@gmail.com>
2019-04-29 23:44:55 +0800

    super basic react frontend

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="display-1">Oliver's Game Deals!</h1>
+        <h3><small className="text-muted">The Best Videogame Deals on the Planet</small></h3>
+      </div>
+  
+    );
+  }
+}
+
+export default Header;
