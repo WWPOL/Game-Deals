b3d9196258b5cf904f4572c9f42f4cf52bdd5b40
Noah Huppert <contact@noahh.io>
2020-04-19 17:26:03 -0400

    added icons8 megaphone icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import megaphoneIcon from "../images/megaphone.png";
+
 const CustomFooter = styled.footer`
   display: flex;
   justify-content: center;
@@ -11,7 +13,31 @@ const CustomFooter = styled.footer`
 `;
 
 const Footer = () => (
-  <CustomFooter>© {new Date().getFullYear()}, Olly G Inc.</CustomFooter>
+  <div
+    style={{
+      'background': 'rebeccapurple',
+      'color': 'white',
+      'padding': '1rem',
+      'text-align': 'center',
+    }}
+  >
+    <div>
+      © {new Date().getFullYear()}, Olly G Inc.
+    </div>
+
+    <div>
+      Megaphone (
+      <img
+        src={megaphoneIcon}
+        alt="Megaphone Icon"
+        style={{
+          width: '1.5rem',
+        }}
+      />
+      ) icon from
+      <a href="https://icons8.com"> Icons8</a>
+    </div>
+  </div>
 );
 
 export default Footer;
