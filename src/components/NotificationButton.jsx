8d8403c98360f569e85ad2ee513c331624635a64
Noah Huppert <contact@noahh.io>
2020-04-21 00:00:40 -0400

    made subscribe / unsubscribe button

--- src/components/NotificationButton.jsx
+++ src/components/NotificationButton.jsx
@@ -12,18 +12,31 @@ const megaphoneIconEl = <img src={megaphoneIcon} alt="Megaphone icon" />;
 const loadingEl = <Spinner animation="grow"></Spinner>;
 
 const NotificationButton = () => {
+  // State
   const setError = useContext(ErrorContext);
 
   const [fcmToken, setFCMToken] = useState(null);
-  const [subscribed, setSubscribed] = useState(false);
+  const [subscribed, stateSetSubscribed] = useState(false);
+  const setSubscribed = (v) => {
+    localStorage.setItem("subscribed", v);
+    stateSetSubscribed(v);
+  };
   const [loading, setLoading] = useState(false);
 
+  // Firebase
   const messaging = firebase.messaging();
   const functions = firebase.functions();
 
-  const buttonTxt = subscribed ? "Unsubscribe From Deal Alerts" :
-                    "Subscribe To Deal Alerts";
-  const leftEl = loading ? loadingEl : megaphoneIconEl;
+  // Set button content conditionally
+  var buttonTxt = "";
+  const leftEl = loading === true ? loadingEl : megaphoneIconEl;
+
+  if (loading) {
+    buttonTxt = subscribed === true ? "Unsubscribing" : "Subscribing";
+  } else {
+    buttonTxt = subscribed === true ? "Unsubscribe From Deal Alerts" :
+                "Subscribe To Deal Alerts";
+  }
 
   // Try to get FCM token
   if (fcmToken === null) {
@@ -31,20 +44,31 @@ const NotificationButton = () => {
       // If token retrieved
       if (token !== null) {
         setFCMToken(token);
+      } else {
+        // If no token retrieved make to set subscription status to false
+        setFCMToken(null);
+        setSubscribed(false);
       }
     }).catch((err) => {
       setError(["Failed to get game deals subscription details", err]);
     });
   } else {
-    // TODO: Get token subscription status
+    // Determine (and maybe update) subscription status
+    var localSubscribed = localStorage.getItem("subscribed") === "true";
+
+    if (localSubscribed !== subscribed) {
+      setSubscribed(localSubscribed);
+    }
+    
     // Handle FCM token refreshes if we have a token
     messaging.onTokenRefresh(() => {
       // Get new token
-      messaging.getToken().then((token) => {
-        // TODO: Handle error from subscribe call
-        return functions.httpsCallable('subscribe')();
-      }).catch((err) => {
+      messaging.getToken().catch((err) => {
         setError(["Failed to get game deals subscription details", err]);
+      }).then((token) => {
+        return functions.httpsCallable("subscribe")();
+      }).catch((err) => {
+        setError(["Failed to renew subscription to deals", err]);
       });
     });
   }
@@ -57,25 +81,32 @@ const NotificationButton = () => {
   const onSubscribeClick = () => {
     setLoading(true);
     
-    if (subscribed) {
+    if (subscribed === true) {
       // Unsubscribe
-      // TODO: Unsubscribe user
+      functions.httpsCallable("unsubscribe")().then(() => {
+        setSubscribed(false);
+        setLoading(false);
+      }).catch((err) => {
+        setError([err.userError, err.internalError]);
+        setLoading(false);
+      });
     } else {
       // Subscribe
+      // 1. Request permissions to send browser notifications
+      // 2. Get the user's FCM token
+      // 3. Call the subscribe Firebase Function
       messaging.requestPermission().catch((err) => {
         setError(["Cannot subscribe to deal alerts without " +
                   "notification permissions", err]);
         setLoading(false);
       }).then(() => {
-        return messaging.getToken();
-      }).then((token) => {
-        return functions.httpsCallable('subscribe')();
-      }).then(() => {
-        setLoading(false);
-        setSubscribed(true);
+        return functions.httpsCallable("subscribe")();
       }).catch((err) => {
         setError([err.userError, err.internalErr]);
         setLoading(false);
+      }).then(() => {
+        setLoading(false);
+        setSubscribed(true);
       });
     }
   };
