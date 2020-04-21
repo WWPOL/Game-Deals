2cacdfe05670f1b9f06a371d9a40c1bcd378a108
Noah Huppert <contact@noahh.io>
2020-04-21 00:00:11 -0400

    finished subscribe and unsubscribe firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,66 @@
+/**
+ * If an error occurs functions return with a non-200 code and the keys:userError
+ * and internalError.
+ */
+
 const functions = require("firebase-functions");
+//const firestore = require("firebase/firestore");
+const admin = require("firebase-admin");
+admin.initializeApp();
+const messagingAdmin = admin.messaging();
 
-// TODO: Subscribe and unsubscribe users to / from topics instead of using firestore
+const topic = "deals";
 
+/**
+ * Subscribe a client to the deals FCM topic.
+ */
 exports.subscribe = functions.https.onCall((data, context) => {
-  const fcmId = context.instanceIdToken;
+  const fcmToken = context.instanceIdToken;
 
-  return firestore.collection("subscriptions").doc(fcmId).set({
-    subscribed: true,
-  }).then(() => {
+  return messagingAdmin.subscribeToTopic([fcmToken], topic).then((resp) => {
     return Promise.resolve({});
-  }).catch((err) => {
-    console.error("Failed to set subscription", err);
-    
-    return Promise.resolve({
-      userError: "Failed to record subscription",
-      internalError: err,
+  }).catch((error) => {
+    throw new HttpsError("internal", "Failed to subscribe", {
+      userError: "Failed to subscribe",
+      internalError: error,
     });
   });
 });
 
 exports.unsubscribe = functions.https.onCall((data, context) => {
-  const fcmId = context.instanceIdToken;
+  const fcmToken = context.instanceIdToken;
 
-  return firestore.collection("subscriptions").doc(fcmId).delete().then(() => {
-      return Promise.resolve({});
+  return messagingAdmin.unsubscribeFromTopic([fcmToken], topic).then((resp) => {
+    return Promise.resolve({});
+  }).catch((error) => {
+    throw new HttpsError("internal", "Failed to unsubscribe", {
+      userError: "Failed to unsubscribe",
+      internalError: error,
+    });
+  });
+});
+
+exports.notify = functions.https.onCall((data, context) => {
+  // Determine if admin
+  const uid = context.auth.uid;
+
+  if (uid === null) {
+    throw new HttpsError("unauthenticated", "Not logged in", {
+      userError: "Not logged in",
+      internalError: "No user ID",
+    });
+  }
+
+  /*
+  return firestore.document("/admins/" + uid).exists().then((res) => {
+    console.log("notify, res=", res);
+    return Promise.resolve({});
   }).catch((err) => {
-    console.error("Failed to delete subscription", err);
-
     return Promise.resolve({
-      userError: "Failed to delete subscription",
+      userError: "Failed authenticate",
       internalError: err,
     });
   });
+  */
+  return {};
 });
