3f4802919cd600badaba47c6c6850724f48a3888
Noah Huppert <contact@noahh.io>
2025-12-29 23:07:52 -0500

    Move SECRET_KEY to environment variable with sensible default
    Allow SECRET_KEY to be overridden via environment variable while providing a safe default for development.

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(=)580+zx6yx&$$or&b+jx^bhdee%0aq6zp2nbc-nd=*+3t+y)'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-change-this-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
