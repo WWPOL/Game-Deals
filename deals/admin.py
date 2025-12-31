c8219e8465d2bc4d26e0d30d6a3bef9d5941a292
Noah Huppert <contact@noahh.io>
2025-12-31 14:30:28 -0500

    Default deal expiration time to 11:59 PM when only date is entered in admin

--- deals/admin.py
+++ deals/admin.py
@@ -1,6 +1,7 @@
 import json
 from django import forms
 from django.contrib import admin
+from django.db import models
 from django.urls import reverse
 from django.utils.html import format_html
 from django.shortcuts import redirect
