5e724fc7567cb07324bf7980213231adcc70c09c
Noah Huppert <contact@noahh.io>
2026-01-10 12:16:02 -0500

    converted mgmt script to use logger

--- common/management/commands/update_site_domain.py
+++ common/management/commands/update_site_domain.py
@@ -1,8 +1,11 @@
+import logging
+
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.contrib.sites.models import Site
 from urllib.parse import urlparse
 
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'Ensures the Site object matches the SITE_URL setting'
@@ -19,38 +22,33 @@ class Command(BaseCommand):
         domain = parsed_url.netloc
         
         # Get or create the site object
-        site, created = Site.objects.get_or_create(
-            pk=site_id,
-            defaults={
-                'domain': domain,
-                'name': domain,  # Use domain as name by default
-            }
-        )
+        site = Site.objects.filter(pk=site_id).first()
+        if site is None:
+          raise ValueError("Site not created yet (you probably need to run initial migrations)")
         
         # Update the site if it already existed but domain differs
-        if not created and (site.domain != domain or site.name != domain):
+        if site.domain != domain or site.name != domain:
             old_domain = site.domain
             old_name = site.name
+
             site.domain = domain
             site.name = domain
             site.save()
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f'Successfully updated Site object (ID={site_id}): '
-                    f'domain changed from "{old_domain}" to "{domain}", '
-                    f'name changed from "{old_name}" to "{domain}"'
-                )
-            )
-        elif created:
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f'Successfully created new Site object (ID={site_id}) '
-                    f'with domain="{domain}" and name="{domain}"'
-                )
+
+            logger.info(
+              "Successfully update Site object (ID=%(site_id)s): domain changed from '%(old_domain)s' to '%(domain)s', name changed from '%(old_name)s' to '%(domain)s'",
+              {
+                "site_id": site_id,
+                "old_domain": old_domain,
+                "old_name": old_name,
+                "domain": domain,
+              }
             )
         else:
-            self.stdout.write(
-                self.style.NOTICE(
-                    f'Site object (ID={site_id}) already has the correct domain and name: "{domain}"'
-                )
-            )
+          logger.info(
+            "Site object (ID= %(site_id)s) already has the correct domain and name: '%(domain)s'",
+            {
+              "site_id": site_id,
+              "domain": domain,
+            },
+          )
