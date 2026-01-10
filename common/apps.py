ea2a0e81c50078501e9ef079399ece214a0ebe7d
Noah Huppert <contact@noahh.io>
2026-01-10 18:15:57 -0500

    added django admin for UserTask

--- common/apps.py
+++ common/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CommonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'common'
+    verbose_name = 'Background Tasks'
